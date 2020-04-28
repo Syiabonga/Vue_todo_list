0950e14513fab81431450728bbba8a65488050e6
Leandro <syiabonga111@gmail.com>
2020-04-28 17:25:44 -0500

    todo-item component created

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,12 +1,20 @@
 <template>
     <div>
-        <h1>Todos</h1>
+        <div v-for="todo in todos" v-bind:key="todo.id">
+            <TodoItem v-bind:todo="todo"/>
+        </div>
     </div>
 </template>
 
 <script>
+import TodoItem from './TodoItem.vue'
+
 export default {
-    name: "Todos"
+    name: "Todos",
+    props: ["todos"],
+    components: {
+        TodoItem,
+    }
 }
 </script>
 
